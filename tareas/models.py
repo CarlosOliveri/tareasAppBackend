9fbe31b3cc62b13ef328d8dfa7e2cb7f3e4bb94a
CarlosOliveri <coliveri@fiuna.edu.py>
2024-05-15 23:13:56 -0400

    API Rest creada, y generacion de proyecto de frontend

--- tareas/models.py
+++ tareas/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Proyect(models.Model):
-    name = models.CharField(max_length = 200)
-    
 class task(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField()
-    proyect = models.ForeignKey(Proyect, on_delete = models.CASCADE)
+    done = models.BooleanField(default = False)
     
+    def __srt__(self):
+        return self.title
--- tareas/views.py
+++ tareas/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializer import TaskSerializer
+from .models import task
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello world</h1>")
 
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = task.objects.all()
